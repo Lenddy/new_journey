cab80a3122caa25865a3aa02eb078b5d64bb6daf
Lenddy <87032108+Lenddy@users.noreply.github.com>
2023-08-26 17:33:31 -0400

    bst folder

--- algos/0_coding_dojo_algos/Trees/Bst/Bst.js
+++ algos/0_coding_dojo_algos/Trees/Bst/Bst.js
@@ -111,4 +111,22 @@ class BinarySearchTree {
 	}
 }
 
+const bst = new BinarySearchTree();
+console.log(bst);
+
+// console.log(bst.isEmpty());
+
+bst.insert(10);
+bst.insert(8);
+bst.insert(13);
+bst.insert(7);
+bst.insert(9);
+bst.insert(11);
+bst.insert(15);
+console.log(bst.min());
+console.log(bst.max());
+
+console.log(bst.contains(11));
+
+
 module.exports = BinarySearchTree;
