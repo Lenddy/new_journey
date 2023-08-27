f64790b7bd75bc966947618dd02949708113ea8b
Lenddy Morales <leme2003@gmail.com>
2023-08-27 12:13:45 -0400

    no change

--- algos/0_coding_dojo_algos/Trees/Bst/Bst.js
+++ algos/0_coding_dojo_algos/Trees/Bst/Bst.js
@@ -48,25 +48,49 @@ class BinarySearchTree {
 			//if the given value is found return true else return false
 			if (current.data !== null) {
 				if (searchVal == current.data) {
-					console.log("is == in the left");
 					return true;
 				} else if (searchVal < current.data) {
-					console.log("is smaller");
 					current = current.left;
 				}
 			}
-
 			if (current.data !== null) {
 				if (searchVal == current.data) {
-					console.log("is == in the right");
 					return true;
 				} else if (searchVal > current.data) {
-					console.log("is bigger");
 					current = current.right;
 				}
 			}
 		}
 		return false;
+	}
+
+	containsRecursive(searchVal, current = this.root) {
+		//! returns a bool
+
+		// make the base case
+		//if the current node is == null that also pull us out of the recursion
+		if (current === null) {
+			return false;
+		}
+		//if the search val is == to the current node data then stop the loop
+		if (searchVal == current.data) {
+			return true;
+		}
+		//find out if the  searchVal is bigger or smaller than the current node data
+		// if it is smaller move to the left
+		if (searchVal < current.data) {
+			return this.containsRecursive(searchVal, current.left);
+		}
+
+		//if  it is bigger that move the right
+		if (searchVal > current.data) {
+			return this.containsRecursive(searchVal, current.right);
+		}
+	}
+
+	// calculate the rage (max - min) from the given startNode
+	range(startNode = this.root) {
+		return this.max() - this.min();
 	}
 
 	insert(val) {
@@ -103,29 +127,70 @@ class BinarySearchTree {
 				}
 			}
 		}
-		// ! you need to put this out side
-		// if the current leaf node is == to null that add the inputted data into that leaf
+	}
 
-		// current.data = node;
-		// return this;
+	// dfs preOrder:  (currNode, lest , right)
+	// converts this BST into an array following depth first search preOrder
+	toArrPreOrder(current = this.root, vals = []) {
+		// check to see if the three is empty and returns the values in the array
+		if (this.isEmpty()) {
+			return vals;
+		}
+		//  checks to see if there is a node to available
+		if (current) {
+			vals.push(current.data); // adds the currents node data to the array
+			this.toArrPreOrder(current.left, vals); // recursively loops until there is no more nodes to the left
+			this.toArrPreOrder(current.right, vals); // recursively loops until there is no more nodes to the right
+		}
+		return vals; // returns the array with the values from left
+	}
+
+	toArrInOrder(current = this.root, vals = []) {
+		if (this.isEmpty()) {
+			return vals;
+		}
+		if (current) {
+			this.toArrInOrder(current.left, vals);
+			vals.push(current.data);
+			this.toArrInOrder(current.right, vals);
+		}
+		// console.log(vals);
+		return vals;
+	}
+	toArrPostOrder(current = this.root, vals = []) {
+		if (this.isEmpty()) {
+			return vals;
+		}
+		if (current) {
+			this.toArrPostOrder(current.left, vals);
+			this.toArrPostOrder(current.right, vals);
+			vals.push(current.data);
+		}
+		// console.log(vals);
+		return vals;
 	}
 }
 
-const bst = new BinarySearchTree();
-console.log(bst);
+// const bst = new BinarySearchTree();
+// console.log(bst);
 
-// console.log(bst.isEmpty());
+// // console.log(bst.isEmpty());
 
-bst.insert(10);
-bst.insert(8);
-bst.insert(13);
-bst.insert(7);
-bst.insert(9);
-bst.insert(11);
-bst.insert(15);
-console.log(bst.min());
-console.log(bst.max());
+// bst.insert(10);
+// bst.insert(8);
+// bst.insert(13);
+// bst.insert(7);
+// bst.insert(9);
+// bst.insert(11);
+// bst.insert(15);
+// console.log(bst);
+// // console.log(bst.min());
+// // console.log(bst.max());
 
-console.log(bst.contains(11));
+// // console.log(bst.contains(11));
+// // console.log(bst.containsRecursive(100));
+// // console.log(bst.range());
+
+// console.log(bst.toArrPreOrder());
 
 module.exports = BinarySearchTree;
